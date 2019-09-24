2bdeb64f7b7f2e5d722ee1301487f15d67fc5e67
Shawn <wangcong26@hotmail.com>
2019-09-23 23:59:58 -0400

    Add files via upload

--- Java/Leetcode_217.java
+++ Java/Leetcode_217.java
@@ -1,70 +1,40 @@
 package leetcode;
 
-import java.util.HashMap;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
-//Contain duplicate
 public class Leetcode_217
 {
 	public boolean containsDuplicate(int[] nums)
 	{
-		HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
-
-		for (int i = 0; i < nums.length; i++)
+		Set<Integer> set = new HashSet<>(nums.length);
+		for (int x : nums)
 		{
-			if (map.containsKey(nums[i]))
-			{
-				map.put(nums[i], map.get(nums[i]) + 1);
-			} else
-			{
-				map.put(nums[i], 1);
-			}
-		}
-
-		for (Integer value : map.values())
-		{
-			if (value > 1)
-			{
+			if (set.contains(x))
 				return true;
-			}
+			set.add(x);
 		}
-
 		return false;
 	}
 
-	public static void main(String[] args)
+	// sort first and the adjacent ones indicate duplicate
+	// This is faster
+	/*
+	 * Time complexity : O(nlogn). 
+	 * Sorting is O(nlogn) and the sweeping is O(n). 
+	 * The entire algorithm is dominated by the sorting step, which is O(nlogn).
+	 * Space complexity : O(1)O(1). Space depends on the sorting implementation which, usually, 
+	 * costs O(1) auxiliary space if heap sort is used.
+	 * */
+	public boolean containsDuplicate2(int[] nums)
 	{
-		int[] test =
-		{ 1, 2, 3, 1 };
-		Leetcode_217 contain = new Leetcode_217();
-		contain.containsDuplicate(test);
-		System.out.println(contain.containsDuplicate(test));
-
-		int[] nums =
-		{ 1, 2, 3, 1 };
-
-		HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
-
-		for (int i = 0; i < nums.length; i++)
+		Arrays.sort(nums);
+		for (int i = 0; i < nums.length - 1; ++i)
 		{
-			if (map.containsKey(nums[i]))
-			{
-				map.put(nums[i], map.get(nums[i]) + 1);
-			} else
-			{
-				map.put(nums[i], 1);
-			}
+			if (nums[i] == nums[i + 1])
+				return true;
 		}
-
-		for (Integer value : map.values())
-		{
-			System.out.println(value);
-		}
-		System.out.println("---------------");
-		for (Integer value : nums)
-		{
-			System.out.println(value);
-		}
-
+		return false;
 	}
-
 }
